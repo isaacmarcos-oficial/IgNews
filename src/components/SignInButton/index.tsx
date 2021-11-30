dfe6bd86829ebbb63ee5863e94acc06f5c2a4bb3
Isaac Marcos <isaacmarcos@scdesign.com.br>
2021-11-30 14:53:46 -0300

    first commit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,31 @@
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+import { signIn, useSession } from 'next-auth/client'
+
+import styles from './styles.module.scss'
+
+export function SignInButton() {
+    const [session] = useSession()
+
+    console.log(session);
+
+    return session ? (
+        <button
+            type="button"
+            className={styles.signInButton}
+        >
+            <FaGithub color="#04D361" />
+            Isaac Marcos
+            <FiX color="#737380" className={ styles.closeIcon }/>
+        </button>
+    ) : (
+        <button
+            type="button"
+            className={styles.signInButton}
+            onClick={() => signIn('github')}
+        >
+            <FaGithub color="#EBA417" />
+            Sign in with Github
+        </button>
+    );
+}
